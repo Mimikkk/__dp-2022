d3504b150fff6bb8201d921a6ae15db91d44c0fa
Hououin Kyouma <inwazzin@gmail.com>
2022-03-22 14:46:24 +0100

    assignment 4 stub

---
+++ assignment-4-monte-carlo/main.c
@@ -0,0 +1,11 @@
+#include <mpi.h>
+
+int main(int argc, char **argv) {
+  MPI_Init(&argc, &argv);
+
+  int size, rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  MPI_Finalize();
+}
